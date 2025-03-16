5b43634cc0762d77885f8cfea2299b056beb2909
ShivM017 <73110@jisedu.or.id>
2025-03-16 16:00:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let totalDonations = 0;
 
+function openModal() {
+    document.getElementById('donation-modal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('donation-modal').style.display = 'none';
+}
+
+function donate() {
+    let amount = document.getElementById('donation-input').value;
+    if (amount && amount > 0) {
+        totalDonations += parseFloat(amount);
+        updateDonationBar();
+        alert("Thank you for donating $" + amount + "!");
+        closeModal();
+    } else {
+        alert("Please enter a valid amount.");
+    }
+}
+
+function updateDonationBar() {
+    let goal = 10000;
+    let percentage = (totalDonations / goal) * 100;
+    document.querySelector('.progress-bar').style.width = percentage + '%';
+    document.getElementById('donation-amount').innerText = "$" + totalDonations;
+}
